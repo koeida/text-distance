c409bfd4ca39a3188acc91ebf40f3e8d1c28e80c
Keegan <keegan@twinoaks.org>
2021-08-01 10:38:21 -0400

    initial import

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+const {distance, closest} = require('fastest-levenshtein')
+const {sampleInput} = require('./sample-input.tsx');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getPairs(l: any[]): object[][] {
+  const pairs:object[][] = [];
+  for(let x = 0; x < l.length; x++) {
+    for(let y = 0; y < l.length; y++) {
+      if (x !== y) {
+        pairs.push([l[x], l[y]]);
+      }
+    }
+  }
+  return pairs;
+}
+
+function calculateAllDistances(s:string[]):void {
+  const linePairs = getPairs(s); 
+}
+
+interface IProps {
+}
+
+interface IState {
+  originalText: string;
+}
+
+class App extends React.Component<IProps, IState> {
+  constructor(props:IProps) {
+    super(props);
+    this.state = {
+      originalText: sampleInput(),
+    };
+  }
+  handleChange = (e:any):void => {
+    this.setState({
+      originalText: e.target.value,
+    });
+  }
+
+  handleSubmit = (e:any):void => {
+    e.preventDefault();
+    const pairs = getPairs(this.state["originalText"].split("\n"));
+    console.log(pairs[0]);
+    console.log(distance(pairs[0][0], pairs[0][1]));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Line Similarity Checker</h1>
+        </header>
+        <form>
+          <label htmlFor="input">Input Text</label>
+          <textarea onChange={this.handleChange} id="input" rows={30} cols={50} value={this.state["originalText"]}/>
+          <button id="input-button" onClick={this.handleSubmit}>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
